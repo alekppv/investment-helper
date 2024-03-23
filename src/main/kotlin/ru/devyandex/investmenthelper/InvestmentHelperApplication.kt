405614e4f1a2d6d98b5db39f577afb3bc74d392a
Alexandr Popov <apopov@telematika.com>
2024-03-23 19:57:32 +0700

    Initial commit

---
+++ src/main/kotlin/ru/devyandex/investmenthelper/InvestmentHelperApplication.kt
@@ -0,0 +1,11 @@
+package ru.devyandex.investmenthelper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InvestmentHelperApplication
+
+fun main(args: Array<String>) {
+	runApplication<InvestmentHelperApplication>(*args)
+}
